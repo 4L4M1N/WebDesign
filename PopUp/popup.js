48b0d7c0f12860f01aadbf18e66c66b96d87392f
4L4M1N <mohammadamin601@gmail.com>
2020-05-21 00:16:50 +0600

    popup added

---
+++ PopUp/popup.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector('.popup-wrapper');
+// const close = document.querySelector('.popup-close');
+// close.addEventListener('click', ()=> {
+//     popup.style.display = 'none';
+// });
+popup.addEventListener('click', ()=> {
+    popup.style.display = 'none';
+});
