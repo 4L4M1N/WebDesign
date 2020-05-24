21af1fe8145dc9ff30fb58c9cb6cd49ae27e6a25
4L4M1N <mohammadamin601@gmail.com>
2020-05-24 13:17:24 +0600

    code format

--- login/login.js
+++ login/login.js
@@ -1,6 +1,5 @@
-const form = document.querySelector('.login-form');
-form.addEventListener('submit', e=> {
-
-    e.preventDefault(); //prevent default action (refresh)
-    //console.log(form.username.value); 
-})
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevent default action (refresh)
+  //console.log(form.username.value);
+});
